7bb689e4f49cff58e12e0b97072f4a10efbb859f
samurrai <samurrrai.r@gmail.com>
2019-03-25 22:39:36 +0600

    Домашняя работа

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(3);
+            list.Add(123);
+            list.Add(123);
+            list.Add(4);
+            list.Add(5);
+            int max = list.Count - 1;
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if (list[max] < list[i])
+                {
+                    max = i;
+                }
+            }
+            Console.WriteLine($"{max} - {list[max]}");
+
+            int sum = 0;
+            foreach (var element in list)
+            {
+                sum += element;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
